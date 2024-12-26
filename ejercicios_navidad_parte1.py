38f23a9a9bccb5974bbf73d2d4df8db279a9b98d
David Duque <davduquediaz@gmail.com>
2024-12-26 18:29:28 +0100

    N reinas OK

--- ejercicios_navidad_parte1.py
+++ ejercicios_navidad_parte1.py
@@ -36,8 +36,30 @@ def sorteo_navidad(regalos, gente)->list:
         lista.append(gente[i])
         lista.append(regalos[i])
     return lista
-    
 
+#ejercicio05
+def cuenta_regresiva(n):
+    for i in range(n, 0, -1):  # Itera desde n hasta 1 en decrementos de 1
+        if i % 3 == 0 and i % 5 == 0:
+            print(f"{i}: []")  # Si es divisible por ambos 3 y 5
+        elif i % 3 == 0:
+            print(f"{i}: *")  # Si es divisible por 3
+        elif i % 5 == 0:
+            print(f"{i}: ^")  # Si es divisible por 5
+        else:
+            print(f"{i}")  # Si no es divisible por 3 ni por 5
+
+#ejercicio06
+def sec_natal(lista_personas):
+    mayores_edad = [nombre for nombre, edad in lista_personas if edad >= 18]
+    mayores_edad.sort()
+    return mayores_edad
+
+#ejercicio07
+#SUDOKU
+
+
+########################################################################################################################
 
 #ejercicios
 stop=False
@@ -59,3 +81,9 @@ while not stop:
             gente=["Juanito", "Jaimito", "Jorgito", "Pepito", "Albertito"]
             print(f"Los regalos y sus dueños son: {str(sorteo_navidad(regalos, gente))}")
         case 5:
+            cuenta_regresiva(15)
+        case 6:
+            gentes = [("Maria", 20), ("Carlos", 17), ("Luis", 18), ("Ana", 25)]
+            resultado = sec_natal(gentes)
+            print(resultado)
+        #case 7:
---
+++ problema_n_reinas.py
@@ -0,0 +1,48 @@
+def tablero(n):
+    return [[0 for _ in range(n)] for _ in range(n)]
+
+def imprime_tablero(tablero):
+    for fila in tablero:
+        print(fila)
+
+def comprueba_casilla(x, y, tablero):
+    n = len(tablero)
+
+    # filas y columnas
+    for i in range(n):
+        if tablero[x][i] == "R" or tablero[i][y] == "R":
+            return False
+
+    # diagonales
+    for i in range(n):
+        for j in range(n):
+            if abs(x - i) == abs(y - j) and tablero[i][j] == "R":
+                return False
+
+    return True
+
+def resuelve_n_reinas(tablero, fila=0):
+    n = len(tablero)
+
+    if fila == n:
+        return True
+
+    for columna in range(n):
+        if comprueba_casilla(fila, columna, tablero):
+            tablero[fila][columna] = "R"
+
+            if resuelve_n_reinas(tablero, fila + 1):
+                return True
+
+            tablero[fila][columna] = 0
+
+    return False
+
+
+n = 8
+tab = tablero(n)
+
+if resuelve_n_reinas(tab):
+    imprime_tablero(tab)
+else:
+    print("No hay solución")
